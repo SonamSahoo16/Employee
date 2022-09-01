b02cd4dba13924662858291b19dcffcd66b06e9c
swadesh-online <swadeshswaroopbehera@gmail.com>
2022-09-01 16:00:28 +0530

    Employee info

---
+++ src/main/java/com/sonam/mindfire/MindfireApplication.java
@@ -0,0 +1,13 @@
+package com.sonam.mindfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindfireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindfireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonam/mindfire/MindfireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonam.mindfire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindfireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
